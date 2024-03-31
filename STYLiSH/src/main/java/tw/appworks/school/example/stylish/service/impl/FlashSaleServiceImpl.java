d06c78b9981b6b2307e9823887789e2c05797115
蔣永寬 <mrzackchiang@gmail.com>
2024-03-31 19:44:53 +0800

    fix prefix in flashsale event api

--- STYLiSH/src/main/java/tw/appworks/school/example/stylish/service/impl/FlashSaleServiceImpl.java
+++ STYLiSH/src/main/java/tw/appworks/school/example/stylish/service/impl/FlashSaleServiceImpl.java
@@ -1,17 +1,23 @@
 package tw.appworks.school.example.stylish.service.impl;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import tw.appworks.school.example.stylish.data.dto.FlashSaleEventDto;
 import tw.appworks.school.example.stylish.data.dto.FlashSaleNoticeDto;
+import tw.appworks.school.example.stylish.data.dto.ProductDto;
 import tw.appworks.school.example.stylish.repository.flashSale.FlashSaleDao;
 import tw.appworks.school.example.stylish.service.FlashSale;
 
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.stream.Collectors;
 
 @Service
 public class FlashSaleServiceImpl implements FlashSale {
     private final FlashSaleDao flashSaleDao;
+
+    @Value("${image.prefix}")
+    private String prefix;
 
     public FlashSaleServiceImpl(FlashSaleDao flashSaleDao) {
         this.flashSaleDao = flashSaleDao;
@@ -27,6 +33,13 @@ public class FlashSaleServiceImpl implements FlashSale {
     public FlashSaleEventDto getUpcomingEvent() {
         // get current time stamp
         Timestamp currentTimeStamp = new Timestamp(new Date().getTime());
-        return flashSaleDao.getFlashSaleAfterDate(currentTimeStamp);
+        FlashSaleEventDto flashSaleEventDto = flashSaleDao.getFlashSaleAfterDate(currentTimeStamp);
+        appendPrefix(flashSaleEventDto.getProduct());
+        return flashSaleEventDto;
+    }
+
+    private void appendPrefix(ProductDto dto) {
+        dto.setMainImage(prefix + dto.getMainImage());
+        dto.setImages(dto.getImages().stream().map(image -> prefix + image).collect(Collectors.toSet()));
     }
 }
