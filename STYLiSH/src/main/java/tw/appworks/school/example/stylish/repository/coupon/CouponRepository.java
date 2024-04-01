2dd71f215a3ac3043fb20758f04eebb28c50aa35
蔣永寬 <mrzackchiang@gmail.com>
2024-04-01 09:50:35 +0800

    coupon chance set as env var

--- STYLiSH/src/main/java/tw/appworks/school/example/stylish/repository/coupon/CouponRepository.java
+++ STYLiSH/src/main/java/tw/appworks/school/example/stylish/repository/coupon/CouponRepository.java
@@ -1,11 +1,11 @@
 package tw.appworks.school.example.stylish.repository.coupon;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import tw.appworks.school.example.stylish.model.coupon.Coupon;
-import tw.appworks.school.example.stylish.model.zodiac.Zodiac;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,78 +14,81 @@ import java.util.Map;
 @Repository
 public class CouponRepository {
 
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
+    @Value("${game.chance}")
+    private Integer gameChance;
 
-	public Coupon getRandomCoupon(int randomId) {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
-		String getCouponSql = "SELECT * FROM coupon WHERE coupon_id = ?";
-		return jdbcTemplate.queryForObject(getCouponSql, new BeanPropertyRowMapper<>(Coupon.class), randomId);
+    public Coupon getRandomCoupon(int randomId) {
 
-	}
+        String getCouponSql = "SELECT * FROM coupon WHERE coupon_id = ?";
+        return jdbcTemplate.queryForObject(getCouponSql, new BeanPropertyRowMapper<>(Coupon.class), randomId);
 
-	public Integer getCouponCount() {
+    }
 
-		String getCouponCountSql = "SELECT COUNT(*) FROM coupon";
-		return jdbcTemplate.queryForObject(getCouponCountSql, Integer.class);
+    public Integer getCouponCount() {
 
-	}
+        String getCouponCountSql = "SELECT COUNT(*) FROM coupon";
+        return jdbcTemplate.queryForObject(getCouponCountSql, Integer.class);
 
-	public Integer getChance(Integer userId) {
+    }
 
-		String getChanceSql = "SELECT chance FROM user_game_chance WHERE user_id = ?";
-		return jdbcTemplate.queryForObject(getChanceSql, Integer.class, userId);
+    public Integer getChance(Integer userId) {
 
-	}
+        String getChanceSql = "SELECT chance FROM user_game_chance WHERE user_id = ?";
+        return jdbcTemplate.queryForObject(getChanceSql, Integer.class, userId);
 
-	public void insertToUserCouponsTable(Integer userId, Coupon coupon) {
+    }
 
-		LocalDateTime currentTime = LocalDateTime.now();
-		LocalDateTime expire_time = currentTime.plusDays(coupon.getExpire_days());
+    public void insertToUserCouponsTable(Integer userId, Coupon coupon) {
 
-		String insertSql = "INSERT INTO user_coupons (user_id, coupon_id, obtain_time, expire_time, coupon_status) VALUES (?, ?, ?, ?, ?)";
-		jdbcTemplate.update(insertSql, userId, coupon.getCoupon_id(), currentTime, expire_time, "0");
-	}
+        LocalDateTime currentTime = LocalDateTime.now();
+        LocalDateTime expire_time = currentTime.plusDays(coupon.getExpire_days());
 
-	public void insertToUserGameChanceTable(Integer userId) {
+        String insertSql = "INSERT INTO user_coupons (user_id, coupon_id, obtain_time, expire_time, coupon_status) VALUES (?, ?, ?, ?, ?)";
+        jdbcTemplate.update(insertSql, userId, coupon.getCoupon_id(), currentTime, expire_time, "0");
+    }
 
-		String insertSql = "INSERT INTO user_game_chance (user_id, chance) VALUES (?, ?)";
-		jdbcTemplate.update(insertSql, userId, 3);
-	}
+    public void insertToUserGameChanceTable(Integer userId) {
 
-	public boolean checkUserId(Integer userId) {
+        String insertSql = "INSERT INTO user_game_chance (user_id, chance) VALUES (?, ?)";
+        jdbcTemplate.update(insertSql, userId, gameChance);
+    }
 
-		String sql = "SELECT COUNT(*) FROM user_game_chance WHERE user_id = ?";
-		Integer count = jdbcTemplate.queryForObject(sql, Integer.class, userId);
-		return count != null && count > 0;
-	}
+    public boolean checkUserId(Integer userId) {
 
-	public Integer deductChanceFromDatabase(Integer userId) {
+        String sql = "SELECT COUNT(*) FROM user_game_chance WHERE user_id = ?";
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, userId);
+        return count != null && count > 0;
+    }
 
-		String getChanceSql = "SELECT chance FROM user_game_chance WHERE user_id = ?";
-		Integer chances = jdbcTemplate.queryForObject(getChanceSql, Integer.class, userId);
+    public Integer deductChanceFromDatabase(Integer userId) {
 
-		if (chances != null && chances > 0) {
-			chances--;
+        String getChanceSql = "SELECT chance FROM user_game_chance WHERE user_id = ?";
+        Integer chances = jdbcTemplate.queryForObject(getChanceSql, Integer.class, userId);
 
-			String updateSql = "UPDATE user_game_chance SET chance = ? WHERE user_id = ?";
-			jdbcTemplate.update(updateSql, chances, userId);
-		}
+        if (chances != null && chances > 0) {
+            chances--;
 
-		return chances;
-	}
+            String updateSql = "UPDATE user_game_chance SET chance = ? WHERE user_id = ?";
+            jdbcTemplate.update(updateSql, chances, userId);
+        }
 
-	public Integer getUserId(String accessToken) {
+        return chances;
+    }
 
-		String getUserIdSql = "SELECT id FROM user WHERE access_token = ?";
-		return jdbcTemplate.queryForObject(getUserIdSql, Integer.class, accessToken);
+    public Integer getUserId(String accessToken) {
 
-	}
+        String getUserIdSql = "SELECT id FROM user WHERE access_token = ?";
+        return jdbcTemplate.queryForObject(getUserIdSql, Integer.class, accessToken);
 
-	public List<Map<String, Object>> getCouponList(Integer userID) {
+    }
 
-		String getCouponListSql = "SELECT * FROM user_coupons LEFT JOIN coupon ON user_coupons.coupon_id = coupon.coupon_id WHERE user_id = ?";
-		return jdbcTemplate.queryForList(getCouponListSql, userID);
+    public List<Map<String, Object>> getCouponList(Integer userID) {
 
-	}
+        String getCouponListSql = "SELECT * FROM user_coupons LEFT JOIN coupon ON user_coupons.coupon_id = coupon.coupon_id WHERE user_id = ?";
+        return jdbcTemplate.queryForList(getCouponListSql, userID);
+
+    }
 }
