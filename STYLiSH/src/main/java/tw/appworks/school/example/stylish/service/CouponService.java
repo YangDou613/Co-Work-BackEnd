8a0d6cba92b5cf3ebd2df05956c23e0060107eac
蔣永寬 <mrzackchiang@gmail.com>
2024-04-01 10:20:53 +0800

    unlimit game chance in coupon gamePage api

--- STYLiSH/src/main/java/tw/appworks/school/example/stylish/service/CouponService.java
+++ STYLiSH/src/main/java/tw/appworks/school/example/stylish/service/CouponService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import tw.appworks.school.example.stylish.model.coupon.Coupon;
 import tw.appworks.school.example.stylish.repository.coupon.CouponRepository;
 
-import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,78 +12,78 @@ import java.util.Map;
 @Service
 public class CouponService {
 
-	@Autowired
-	private CouponRepository couponRepository;
+    @Autowired
+    private CouponRepository couponRepository;
 
-	public Map<String, Object> getRandomCouponAndChance(String accessToken) {
+    public Map<String, Object> getRandomCouponAndChance(String accessToken) {
 
-		Integer userId = userId(accessToken); // Get user id
+        Integer userId = userId(accessToken); // Get user id
 
-		// Get
-		Map<String, Object> response = new HashMap<>();
-		Coupon coupon = RandomCoupon(); // Random coupon
+        // Get
+        Map<String, Object> response = new HashMap<>();
+        Coupon coupon = RandomCoupon(); // Random coupon
 
-		// Insert to user_coupons table and user_game_chance table
-		addToDatabase(userId, coupon);
+        // Insert to user_coupons table and user_game_chance table
+        addToDatabase(userId, coupon);
 
-		Integer chance = Chance(accessToken); // Game chance
-		response.put("coupon", coupon);
-		response.put("game_chance", chance);
+//        Integer chance = Chance(accessToken); // Game chance
+        response.put("coupon", coupon);
+//        response.put("game_chance", chance);
 
-		return response;
-	}
+        return response;
+    }
 
-	public void addToDatabase(Integer userId, Coupon coupon) {
-		couponRepository.insertToUserCouponsTable(userId, coupon);
+    public void addToDatabase(Integer userId, Coupon coupon) {
+        couponRepository.insertToUserCouponsTable(userId, coupon);
 
-		boolean isUserIdInUserGameChanceTable = couponRepository.checkUserId(userId);
-		if (!isUserIdInUserGameChanceTable) {
-			couponRepository.insertToUserGameChanceTable(userId);
-		}
-	}
+//		boolean isUserIdInUserGameChanceTable = couponRepository.checkUserId(userId);
+//		if (!isUserIdInUserGameChanceTable) {
+//			couponRepository.insertToUserGameChanceTable(userId);
+//		}
+    }
 
-	public Integer deductOneChance(String accessToken) {
+    public Integer deductOneChance(String accessToken) {
 
-		Integer userId = userId(accessToken);
-		return couponRepository.deductChanceFromDatabase(userId);
-	}
+        Integer userId = userId(accessToken);
+        return couponRepository.deductChanceFromDatabase(userId);
+    }
 
-	public Map<String, Object> getCouponList(String accessToken) {
+    public Map<String, Object> getCouponList(String accessToken) {
 
-		Map<String, Object> response = new HashMap<>();
-		Integer userId = userId(accessToken); // User id
-		List<Map<String, Object>> couponList = couponList(userId); // Coupon list
-		response.put("user_id", userId);
-		response.put("coupons", couponList);
-		return response;
-	}
+        Map<String, Object> response = new HashMap<>();
+        Integer userId = userId(accessToken); // User id
+        List<Map<String, Object>> couponList = couponList(userId); // Coupon list
+        response.put("user_id", userId);
+        response.put("coupons", couponList);
+        return response;
+    }
 
-	public Coupon RandomCoupon() {
+    public Coupon RandomCoupon() {
 
-		int couponCount = couponRepository.getCouponCount();
+        int couponCount = couponRepository.getCouponCount();
 
-		// Random id
-		int randomId = 0;
-		randomId = (int)(Math.random() * couponCount) + 1;
+        // Random id
+        int randomId = 0;
+        randomId = (int) (Math.random() * couponCount) + 1;
 
-		return couponRepository.getRandomCoupon(randomId);
-	}
+        return couponRepository.getRandomCoupon(randomId);
+    }
 
-	public Integer Chance(String accessToken) {
+    public Integer Chance(String accessToken) {
 
-		// Get user id
-		Integer userId = userId(accessToken);
+        // Get user id
+        Integer userId = userId(accessToken);
 
-		// Get chance
-		return couponRepository.getChance(userId);
+        // Get chance
+        return couponRepository.getChance(userId);
 
-	}
+    }
 
-	public Integer userId(String accessToken) {
-		return couponRepository.getUserId(accessToken);
-	}
+    public Integer userId(String accessToken) {
+        return couponRepository.getUserId(accessToken);
+    }
 
-	public List<Map<String, Object>> couponList(Integer userID) {
-		return couponRepository.getCouponList(userID);
-	}
+    public List<Map<String, Object>> couponList(Integer userID) {
+        return couponRepository.getCouponList(userID);
+    }
 }
