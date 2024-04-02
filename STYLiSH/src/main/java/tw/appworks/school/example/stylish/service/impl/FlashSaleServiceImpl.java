60c7c465477cef0a3d2ffa2bda55c30f7e04db19
蔣永寬 <mrzackchiang@gmail.com>
2024-04-02 09:05:00 +0800

    prevent redis connection fail crash the server

--- STYLiSH/src/main/java/tw/appworks/school/example/stylish/service/impl/FlashSaleServiceImpl.java
+++ STYLiSH/src/main/java/tw/appworks/school/example/stylish/service/impl/FlashSaleServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.exceptions.JedisConnectionException;
 import tw.appworks.school.example.stylish.data.dto.FlashSaleEventDto;
 import tw.appworks.school.example.stylish.data.dto.FlashSaleNoticeDto;
 import tw.appworks.school.example.stylish.data.dto.ProductDto;
@@ -64,29 +65,35 @@ public class FlashSaleServiceImpl implements FlashSale {
     }
 
     private boolean checkStock(Long flashSaleId) {
-        Jedis jedis = new Jedis(host, port);
 
-        // Check if key exists
-        Integer initialStock = flashSaleDao.getStockByFlashSaleId(flashSaleId);
+        try {
+            Jedis jedis = new Jedis(host, port);
+            // Check if key exists
+            Integer initialStock = flashSaleDao.getStockByFlashSaleId(flashSaleId);
 
-        Long result = jedis.setnx(String.valueOf(flashSaleId), String.valueOf(initialStock));
+            Long result = jedis.setnx(String.valueOf(flashSaleId), String.valueOf(initialStock));
 
-        // If the key already exists, result will be 0, meaning the key was not set
-        if (result == 1) {
-            // Key didn't exist before, set initial stock
-            log.info("Initial stock set for flash sale: " + initialStock);
-        }
+            // If the key already exists, result will be 0, meaning the key was not set
+            if (result == 1) {
+                // Key didn't exist before, set initial stock
+                log.info("Initial stock set for flash sale: " + initialStock);
+            }
 
-        Long newCount = jedis.decr(String.valueOf(flashSaleId));
-        // Close the connection
-        jedis.close();
-        // Check if the counter is already at zero
-        if (newCount < 0) {
-            log.info("Stock below 0, flash sale product sold out!");
-            return false;
-        } else {
-            System.out.println("Product purchased successfully. Remaining stock: " + newCount);
-            log.info("Stock decrease 1, remaining stock: " + newCount);
+            Long newCount = jedis.decr(String.valueOf(flashSaleId));
+            // Close the connection
+            jedis.close();
+            // Check if the counter is already at zero
+            if (newCount < 0) {
+                log.info("Stock below 0, flash sale product sold out!");
+                return false;
+            } else {
+                System.out.println("Product purchased successfully. Remaining stock: " + newCount);
+                log.info("Stock decrease 1, remaining stock: " + newCount);
+                return true;
+            }
+        } catch (JedisConnectionException e) {
+            // let the db handle
+            log.warn("Redis connection failed");
             return true;
         }
     }
